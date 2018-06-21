d97d1b6880cbeaba9d341f88a125302565f86c9f
Sergio Juarez <sergio@childcareon.com>
2018-06-21 21:20:53 +0200

    version 1.0.1

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
+let notificationColor;
+
+exports.init = (params) => {
+  if(typeof params === 'undefined') return;
+  if(params.hasOwnProperty('notificationColor')) notificationColor = params.notificationColor;
+};
+
 exports.addLeadingZeros = (num) => {
   if(parseInt(num) < 10) return '0'+num;
   return num;
+};
+
+exports.setNotification = (title,body,tag, extra) =>{
+  let not = {
+    notification:{
+      "title":title,
+      "body":body,
+      "icon":"notification_icon",
+      "color":notificationColor,
+      "sound":"default"
+    }
+  };
+  if(typeof tag !== 'undefined') not.notification.tag = tag;
+  if(typeof extra !== 'undefined') not.data = extra;
+  return not;
 };
